5a00af5e809c38027fa7a65ce3c9d18289d1551b
Manuel Aulló Pérez <manuel.aullo@cybercom.com>
2018-03-26 13:59:41 +0300

    webpack production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 // entry -> output
 // module.exports (node.js) expose something (object) to another file
 const path = require("path");
-module.exports = {
-	entry: "./src/app.js",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js"
-	},// loader
-	module: {
-		rules: [{
-			loader: "babel-loader",
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				"style-loader",
-				"css-loader",
-				"sass-loader"
-			]
-		}]
-	},// cheap-module-eval-source-map - shows in which component there is an error
-	devtool: "cheap-module-eval-source-map",
-	devServer: {// configurate dev-server
-		contentBase: path.join(__dirname, "public"),
-		port: 9090,
-		historyApiFallback: true
-	}
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-};
+module.exports = (env) => {
+	const isProduction = env === "production";
+	const CSSExtract = new ExtractTextPlugin("styles.css");
+	return {
+		entry: "./src/app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js"
+		},// loader
+		module: {
+			rules: [{
+				loader: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: CSSExtract.extract({
+					use:[
+							{
+								loader: "css-loader",
+								options: {
+									sourceMap: true
+								}
+							},
+							{
+								loader: "sass-loader",
+								options: {
+									sourceMap: true
+								}
+							}
+					]
+				})
+			}]
+		},
+		plugins: [
+			CSSExtract
+		], // cheap-module-eval-source-map - shows in which component there is an error
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		devServer: {// configurate dev-server
+			contentBase: path.join(__dirname, "public"),
+			port: 9090,
+			historyApiFallback: true
+		}
+	};
+
+
+}
